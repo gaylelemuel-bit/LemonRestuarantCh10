4f53dfc49fe2a1f229431f69dc02d1e4c4bff6ec
Lemuel Gayle <gaylelemuel@gmail.com>
2026-06-05 15:55:37 -0500

    assignment 2

---
+++ LemonRestuarantCh10/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRestuarantCh10
+//
+//  Created by Lemuel Gayle on 6/5/26.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
